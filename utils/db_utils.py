4059adbe7bab8c52570edec135358949697cbd99
Wenlock123 <whaseley9@gmail.com>
2026-03-07 21:30:01 +0700

    Update db_utils.py

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -3,7 +3,7 @@ import os
 
 def extract_chromadb():
 
-    if not os.path.exists("chromadb_database"):
+    if not os.path.exists("chroma_db"):
 
         with zipfile.ZipFile("chroma_db.zip", "r") as zip_ref:
             zip_ref.extractall()
