da2e3b278300533f475992b53a99d12a44b56a2f
Wenlock123 <whaseley9@gmail.com>
2026-03-07 20:29:47 +0700

    Create whisper_utils.py

---
+++ utils/whisper_utils.py
@@ -0,0 +1,12 @@
+import whisper
+
+model = whisper.load_model("medium")
+
+def speech_to_text(audio_path):
+
+    result = model.transcribe(
+        audio_path,
+        language="th"
+    )
+
+    return result["text"]
