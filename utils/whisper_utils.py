aae7a26ba5195e48ed2b0b3653457efbc636d392
Wenlock123 <whaseley9@gmail.com>
2026-03-09 17:16:55 +0700

    Update whisper_utils.py

--- utils/whisper_utils.py
+++ utils/whisper_utils.py
@@ -1,11 +1,20 @@
 import whisper
+import tempfile
 
+# โหลดโมเดลครั้งเดียว
 model = whisper.load_model("small")
 
-def speech_to_text(audio_path):
 
+def speech_to_text(audio_bytes):
+
+    # สร้างไฟล์ชั่วคราวสำหรับเก็บเสียง
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as tmp:
+        tmp.write(audio_bytes)
+        temp_audio_path = tmp.name
+
+    # ให้ whisper ถอดเสียง
     result = model.transcribe(
-        audio_path,
+        temp_audio_path,
         language="th"
     )
 
